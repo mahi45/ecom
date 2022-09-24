94a482963bf47f7bad0bfd890884be1658c419d8
Mohidul Hasan <mahi110145@gmail.com>
2022-09-24 19:56:43 +0600

    Frontend and backend template setup

---
+++ resources/views/backend/layouts/inc/footer.blade.php
@@ -0,0 +1,14 @@
+        <!-- Layout Footer Start -->
+        <footer>
+            <div class="footer-content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-sm-6">
+                            <p class="mb-0 text-muted text-medium">All Rights Reserved by Mohidul
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Layout Footer End -->
---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('backend.layouts.master')
+
+@section('title', 'Dashboard')
+
+@section('admin_content')
+    <div class="row">
+        Starts from here
+    </div>
+@endsection
