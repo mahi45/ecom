73d44195514fe4729445e63624c87d76a9533a45
Mohidul Hasan <mahi110145@gmail.com>
2022-10-23 12:28:41 +0600

    custom middleware for admin and customer

---
+++ app/Http/Middleware/IsCustomer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Route;
+
+class IsCustomer
+{
+    protected $auth;
+    protected $route;
+
+    public function __construct(Guard $auth, Route $route)
+    {
+        $this->auth = $auth;
+        $this->route = $route;
+    }
+    public function handle(Request $request, Closure $next)
+    {
+        if($this->auth->user()->is_system_admin == 1){
+            return response('Access Denied, you are not admin user', 401);
+        }
+        return $next($request);
+    }
+}
